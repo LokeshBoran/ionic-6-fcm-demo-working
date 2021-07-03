53ae4b04d2c737d4d65b7f89791ecc6efc7d7b71
Lokesh Boran <erlokeshboran@gmail.com>
2021-07-04 02:56:49 +0530

    buildable and working code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FCMNG } from 'fcm-ng';
+import { Platform } from '@ionic/angular';
+import { CommonService } from './common.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private fcm: FCMNG, private platform: Platform, private rootData: CommonService) {
+    this.platform.ready().then(() => {
+      this.fcm.onNotification().subscribe(data => {
+        if (data.wasTapped) {
+          this.rootData.updateMessage(JSON.stringify(data));
+          console.log('Received in background');
+        } else {
+          console.log('Received in foreground');
+        };
+      });
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../common.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit  {
+  currentValue: string;
 
-  constructor() {}
+  constructor(private rootData: CommonService) { }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.rootData.currentData.subscribe(currentData => this.currentValue = currentData);
+  }
 
 }
